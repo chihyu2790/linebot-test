90fa85e1ec5e559f44312e275501327881f72b58
chihyu2790 <aaa2790aaa@gmail.com>
2022-05-16 23:07:15 +0800

    觀察consolelog

--- data.js
+++ data.js
@@ -8,11 +8,16 @@ const courses = []
 // 串一張圖
 const fetchData = async () => {
   try {
+    // 用axios取出，並使用data的部分
     const { data } = await axios.get('https://wdaweb.github.io/')
+    // 可用jq語法去解析html
     const $ = cheerio.load(data)
+    // 片歷內容
     $('#general .col-md-3').each(function () {
+      // 推送到陣列裡
       courses.push(
         [
+          // 圖片文字處理
           'https://wdaweb.github.io/' + $(this).find('img').attr('src').slice(2),
           ...$(this).text().replace(/\t/g, '').split('\n').filter(text => text.length > 0)
         ]
@@ -22,7 +27,7 @@ const fetchData = async () => {
     console.log(error)
   }
 }
-
+// 傳入event物件
 const replyCourses = (event) => {
   const bubbles = courses.map(course => {
     const bubble = JSON.parse(JSON.stringify(template))
@@ -31,6 +36,7 @@ const replyCourses = (event) => {
     bubble.body.contents[1].text = course[2]
     return bubble
   })
+  console.log('hello here' + typeof (bubbles))
   console.log(JSON.stringify(bubbles, null, 2))
   fs.writeFileSync('bubbles.json', JSON.stringify(bubbles, null, 2))
   event.reply([
