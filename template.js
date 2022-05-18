c3469fd32c39c768b25fe49623de5637202b16b5
chihyu2790 <aaa2790aaa@gmail.com>
2022-05-18 22:16:19 +0800

    嘗試使用新的flex版型

--- template.js
+++ template.js
@@ -1,48 +1,54 @@
-// export default {
-//   type: 'bubble',
-//   hero: {
-//     type: 'image',
-//     url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
-//     size: 'full',
-//     aspectRatio: '20:13',
-//     aspectMode: 'cover',
-//     action: {
-//       type: 'uri',
-//       uri: "http://linecorp.com/"
-//     }
-//   },
-//   body: {
-//     type: 'box',
-//     layout: 'vertical',
-//     spacing: 'md',
-//     contents: [
-//       {
-//         type: 'text',
-//         text: 'Brown Cafe',
-//         wrap: true,
-//         weight: 'bold',
-//         gravity: 'center',
-//         size: 'xl'
-//       },
-//       {
-//         type: 'box',
-//         layout: 'vertical',
-//         margin: 'lg',
-//         spacing: 'sm',
-//         text: 'hello, world'
-//         contents: [
-//           {
-//             type: 'text',
-//             "text": "Date",
-//             color: '#aaaaaa',
-//             size: 'sm',
-//             'flex': 1
-//           },
-//         ]
-//       }
-//     ]
-//   }
-// }
+export default {
+  type: 'bubble',
+  hero: {
+    type: 'image',
+    url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+    size: 'full',
+    aspectRatio: '20:13',
+    aspectMode: 'cover',
+    action: {
+      type: 'uri',
+      uri: 'http://linecorp.com/'
+    }
+  },
+  body: {
+    type: 'box',
+    layout: 'vertical',
+    spacing: 'md',
+    contents: [
+      {
+        type: 'text',
+        text: 'BROWNS ADVENTURE IN MOVIE',
+        wrap: true,
+        weight: 'bold',
+        gravity: 'center',
+        size: 'xl'
+      },
+      {
+        type: 'box',
+        layout: 'vertical',
+        margin: 'lg',
+        spacing: 'sm',
+        contents: [
+          {
+            type: 'text',
+            text: '類別',
+            color: '#aaaaaa',
+            size: 'sm',
+            flex: 1
+          }, {
+            type: 'text',
+            text: '貓',
+            wrap: true,
+            color: '#666666',
+            size: 'sm',
+            flex: 4
+          }
+        ]
+      }
+    ]
+  }
+}
 
 // {
 //   "type": "bubble",
@@ -148,29 +154,29 @@
 //   }
 // }
 
-export default {
-  type: 'bubble',
-  hero: {
-    type: 'image',
-    url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
-    size: 'full',
-    aspectRatio: '20:13',
-    aspectMode: 'cover'
-  },
-  body: {
-    type: 'box',
-    layout: 'vertical',
-    contents: [
-      {
-        type: 'text',
-        text: 'Brown Cafe',
-        weight: 'bold',
-        size: 'xl'
-      },
-      {
-        type: 'text',
-        text: 'hello, world'
-      }
-    ]
-  }
-}
+// export default {
+//   type: 'bubble',
+//   hero: {
+//     type: 'image',
+//     url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+//     size: 'full',
+//     aspectRatio: '20:13',
+//     aspectMode: 'cover'
+//   },
+//   body: {
+//     type: 'box',
+//     layout: 'vertical',
+//     contents: [
+//       {
+//         type: 'text',
+//         text: 'Brown Cafe',
+//         weight: 'bold',
+//         size: 'xl'
+//       },
+//       {
+//         type: 'text',
+//         text: 'hello, world'
+//       }
+//     ]
+//   }
+// }
